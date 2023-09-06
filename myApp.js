87384fa968c2756373d4783e233c81153bbc9791
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-09-06 16:57:15 +0330

    express static files added

--- myApp.js
+++ myApp.js
@@ -3,9 +3,11 @@ let app = express();
 
 console.log("Hello World")
 absolutePath = __dirname + '/views/index.html'
+publicAbsolutePath = __dirname + "/public"
 app.get("/", function(req, res) {
     res.sendFile(absolutePath)
 })
+app.use("/public", express.static(publicAbsolutePath))
 
 
 
