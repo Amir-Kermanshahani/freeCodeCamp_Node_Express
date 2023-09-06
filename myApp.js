55c2798d001f7b1a91dca2f3bfa58735a46d78f4
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-09-06 19:50:36 +0330

    url parameter added

--- myApp.js
+++ myApp.js
@@ -35,7 +35,11 @@ app.get('/now', function(req, res, next) {
         time: req.time
     })
 })
-
+app.get("/:word/echo", function(req, res) {
+    res.json({
+        echo: req.params.word
+    })
+})
 
 
 
